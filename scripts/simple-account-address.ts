b5eb96919bb7f8e2f2aa0bbdffe93a1635273cf8
ququzone <xueping.yang@gmail.com>
2023-02-02 21:48:37 +0800

    complete compute address script

---
+++ scripts/simple-account-address.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat"
+
+import { SimpleAccountFactory } from "../typechain/contracts/samples/SimpleAccountFactory";
+
+async function main() {
+    const factory = (await ethers.getContract("SimpleAccountFactory")) as SimpleAccountFactory
+
+    const owner = new ethers.Wallet(process.env.OWNER!)
+    
+    const address = await factory.getAddress(owner.address, 0)
+
+    console.log(`${owner.address} owned account address: ${address}`)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
