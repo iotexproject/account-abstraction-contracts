4ec3b31f71d445d7c26dae77304ccb5d982eacb0
ququzone <xueping.yang@gmail.com>
2023-02-06 19:09:39 +0800

    add secp256r1 account factory

---
+++ contracts/samples/secp256r1/P256AccountFactory.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Create2.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+import "./P256Account.sol";
+import "./ISecp256r1.sol";
+
+contract SimpleAccountFactory {
+    P256Account public immutable accountImplementation;
+
+    constructor(IEntryPoint _entryPoint, ISecp256r1 _validator) {
+        accountImplementation = new P256Account(_entryPoint, _validator);
+    }
+
+    function createAccount(bytes calldata publicKey, uint256 salt)
+        public
+        returns (P256Account ret)
+    {
+        address addr = getAddress(publicKey, salt);
+        uint256 codeSize = addr.code.length;
+        if (codeSize > 0) {
+            return P256Account(payable(addr));
+        }
+        ret = P256Account(
+            payable(
+                new ERC1967Proxy{salt: bytes32(salt)}(
+                    address(accountImplementation),
+                    abi.encodeCall(P256Account.initialize, publicKey)
+                )
+            )
+        );
+    }
+
+    function getAddress(bytes calldata publicKey, uint256 salt) public view returns (address) {
+        return
+            Create2.computeAddress(
+                bytes32(salt),
+                keccak256(
+                    abi.encodePacked(
+                        type(ERC1967Proxy).creationCode,
+                        abi.encode(
+                            address(accountImplementation),
+                            abi.encodeCall(P256Account.initialize, publicKey)
+                        )
+                    )
+                )
+            );
+    }
+}
