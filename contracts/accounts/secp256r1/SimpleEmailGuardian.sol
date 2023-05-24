98d713e7217515b0e6abad642d993cc26cd6c873
ququzone <xueping.yang@gmail.com>
2023-05-24 23:47:49 +0800

    redeploy contracts

--- contracts/accounts/secp256r1/SimpleEmailGuardian.sol
+++ contracts/accounts/secp256r1/SimpleEmailGuardian.sol
@@ -5,10 +5,16 @@ import "../../interfaces/IEmailGuardian.sol";
 
 contract SimpleEmailGuardian is IEmailGuardian {
     mapping(bytes32 => address) private users;
+    mapping(address => bytes32) private emails;
 
     function register(bytes32 email) external override {
         require(users[email] == address(0), "already bind");
+        bytes32 preEmail = emails[msg.sender];
+        if (preEmail != 0) {
+            users[preEmail] = address(0);
+        }
         users[email] = msg.sender;
+        emails[msg.sender] = email;
     }
 
     function account(bytes32 email) external view override returns (address) {
