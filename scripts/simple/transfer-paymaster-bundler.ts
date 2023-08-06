72a62778946a6877e54b7afea0439e08fca1bb13
ququzone <xueping.yang@gmail.com>
2023-08-06 23:46:17 +0800

    complete use paymaster and bundler script

--- scripts/simple/transfer-paymaster-bundler.ts
+++ scripts/simple/transfer-paymaster-bundler.ts
@@ -2,6 +2,7 @@ import { Client } from "userop"
 import { ethers } from "hardhat"
 import { Presets } from "userop"
 import { EntryPoint } from "@account-abstraction/contracts"
+import { JsonRpcProvider } from "@ethersproject/providers"
 
 async function main() {
     const rpc = "https://babel-api.testnet.iotex.io"
@@ -9,34 +10,37 @@ async function main() {
     const bundlerRpc = "http://localhost:4337"
     const entryPoint = (await ethers.getContract("EntryPoint")) as EntryPoint
     const accountFactory = await ethers.getContract("SimpleAccountFactory")
+    const accountTpl = await ethers.getContractFactory("P256Account")
     const client = await Client.init(rpc, {
         entryPoint: entryPoint.address,
         overrideBundlerRpc: bundlerRpc,
     })
     const signer = new ethers.Wallet(process.env.PRIVATE_KEY!, ethers.provider)
+    const callData = accountTpl.interface.encodeFunctionData("execute", [
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+        ethers.utils.parseEther("0.1"),
+        "0x",
+    ])
 
     const simpleAccountBuilder = await Presets.Builder.SimpleAccount.init(signer, rpc, {
         overrideBundlerRpc: bundlerRpc,
         factory: accountFactory.address,
         entryPoint: entryPoint.address,
         salt: 1,
+        paymasterMiddleware: Presets.Middleware.verifyingPaymaster(
+            // paymaster rpc
+            "http://localhost:8888/rpc/1234567890",
+            ""
+        ),
     })
+    simpleAccountBuilder.setCallData(callData)
 
     const account = simpleAccountBuilder.getSender()
 
-    const stake = await entryPoint.balanceOf(account)
-    if (stake.isZero()) {
-        console.log(`deposit gas for account ${account}`)
-        const tx = await entryPoint
-            .connect(signer) // use signer for stake gas
-            .depositTo(account, { value: ethers.utils.parseEther("10") })
-        await tx.wait()
-    }
-
     const response = await client.sendUserOperation(simpleAccountBuilder)
-    console.log(`Create account ${account} ophash: ${response.userOpHash}`)
+    console.log(`Transfer ${account} ophash: ${response.userOpHash}`)
     const userOperationEvent = await response.wait()
-    console.log(`Create account ${account} txhash: ${userOperationEvent?.transactionHash}`)
+    console.log(`Transfer ${account} txhash: ${userOperationEvent?.transactionHash}`)
 }
 
 main()
