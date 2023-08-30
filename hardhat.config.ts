a0ac6319d669f065062c92c59844f3703e6f4977
ququzone <xueping.yang@gmail.com>
2023-08-30 12:10:46 +0800

    refactor email bind script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -63,6 +63,7 @@ const config: HardhatUserConfig = {
         },
         localhost: {
             url: "http://127.0.0.1:8545",
+            accounts: accounts,
         },
         mainnet: {
             url: MAINNET_RPC_URL,
--- scripts/secp256r1/add-email-paymaster-bundler.ts
+++ scripts/secp256r1/add-email-paymaster-bundler.ts
@@ -2,70 +2,63 @@ import { ethers } from "hardhat"
 import * as fs from "fs"
 import * as path from "path"
 import ecPem from "ec-pem"
-import { resolveProperties } from "ethers/lib/utils"
 
 import { P256AccountFactory } from "../../typechain"
 import { EntryPoint } from "@account-abstraction/contracts"
-import { deepHexlify, fillUserOp, signOp } from "../utils"
-import { P2565Signer } from "./signer"
-import { JsonRpcProvider } from "@ethersproject/providers"
+import { Client } from "userop"
+import { P256Account } from "../userop/p256-account"
+import { P2565Signer } from "../userop/p256-signature"
 
+// TODO need create account first
 async function main() {
-    const factory = (await ethers.getContract("P256AccountFactory")) as P256AccountFactory
+    const rpc = "https://babel-api.testnet.iotex.io"
+    const bundlerRpc = "https://bundler.testnet.w3bstream.com"
+    const accountFactory = (await ethers.getContract("P256AccountFactory")) as P256AccountFactory
     const accountTpl = await ethers.getContractFactory("P256Account")
     const entryPoint = (await ethers.getContract("EntryPoint")) as EntryPoint
-    const paymaster = new JsonRpcProvider("https://paymaster.testnet.iotex.io")
-    const bundler = new JsonRpcProvider("https://bundler.testnet.iotex.io")
+    const client = await Client.init(rpc, {
+        entryPoint: entryPoint.address,
+        overrideBundlerRpc: bundlerRpc,
+    })
 
     const keyContent = fs.readFileSync(path.join(__dirname, "key.pem"))
     const keyPair = ecPem.loadPrivateKey(keyContent)
+    const signer = new P2565Signer(keyPair)
 
-    const publicKey = "0x" + keyPair.getPublicKey("hex").substring(2)
-
-    const index = 0
-    const account = await factory.getAddress(publicKey, index)
-
-    console.log(`add email for ${account}...`)
+    const accountBuilder = await P256Account.init(signer, rpc, {
+        overrideBundlerRpc: bundlerRpc,
+        factory: accountFactory.address,
+        entryPoint: entryPoint.address,
+        salt: 1,
+    })
+    const account = accountBuilder.getSender()
 
     const callData = accountTpl.interface.encodeFunctionData("execute", [
         account,
         0,
-        "0x99a4453183a3351928007a2dd3f6c9385984d49c2b2761c0cb7110ba9a0a80acf3719597",
+        accountTpl.interface.encodeFunctionData("addEmailGuardian", [
+            // email
+            "0x37c46ee7d1e7eadf71a0aa183942a62007c6b96d9300b177e3b4f721c9989f2c",
+            // signature
+            "0x7d76948c532ad201ade9eab9551759ff3264d6d1db7f76385b756cd92ee4b749379c3069cb4ce88f4ea1710fc8a1b4c98825115f2d616ab2032e77324906b2901c"
+        ]),
     ])
+    accountBuilder.setCallData(callData)
 
-    const transferOp = {
-        sender: account,
-        callData,
-        preVerificationGas: 50000,
+    const stake = await entryPoint.balanceOf(account)
+    if (stake.isZero()) {
+        console.log(`Stake gas for account ${account}`)
+        const [owner] = await ethers.getSigners()
+        const tx = await entryPoint
+            .connect(owner)
+            .depositTo(account, { value: ethers.utils.parseEther("2") })
+        await tx.wait()
     }
 
-    const fullCreateOp = await fillUserOp(transferOp, entryPoint)
-    let hexifiedUserOp = deepHexlify(await resolveProperties(fullCreateOp))
-    let result = await paymaster.send("eth_signVerifyingPaymaster", [hexifiedUserOp])
-
-    fullCreateOp.paymasterAndData = result
-
-    const chainId = (await ethers.provider.getNetwork()).chainId
-    const signedOp = await signOp(
-        fullCreateOp,
-        entryPoint.address,
-        chainId,
-        new P2565Signer(keyPair)
-    )
-
-    const err = await entryPoint.callStatic.simulateValidation(signedOp).catch((e) => e)
-    if (err.errorName === "FailedOp") {
-        console.error(`simulate op error ${err.errorArgs.at(-1)}`)
-        return
-    } else if (err.errorName !== "ValidationResult") {
-        console.error(`unknow error ${err}`)
-        return
-    }
-    console.log(`simulate op success`)
-
-    hexifiedUserOp = deepHexlify(await resolveProperties(signedOp))
-    result = await bundler.send("eth_sendUserOperation", [hexifiedUserOp, entryPoint.address])
-    console.log(`transfer use bundler success opHash: ${result}`)
+    const response = await client.sendUserOperation(accountBuilder)
+    console.log(`Add email guardian to account ${account} ophash: ${response.userOpHash}`)
+    const userOperationEvent = await response.wait()
+    console.log(`Add email guardian to account ${account} txhash: ${userOperationEvent?.transactionHash}`)
 }
 
 main()
